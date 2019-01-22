0fc86d91bf4c3c89ec1a43c44ba1c1be258f76bb
Brooke Perry and Devin Sweeting <brooke@>
2019-01-22 11:59:29 -0800

    create recipe box app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-box';
+  currentFocus: string = 'A box for all your recipes'
+
+  recipes: Recipe[] = [
+    new Recipe('Mac & Cheese', 'A perfect blend of fine cheddar and noodles', ['cheddar', 'noodles', 'bacon bits', 'truffles']),
+    new Recipe('Banana Bread', 'A harmonious blend of banana and bread', ['banana', 'flour', 'almond milk', 'butter', 'maple syrup'])
+  ]
+
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,4 @@
+export class Recipe {
+  public served: boolean = false;
+  constructor(public title: string, public description: string, public ingredients: array) { }
+}
