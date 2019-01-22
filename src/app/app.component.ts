949fdc1b87e0f0a0b21c2f164e1de12862319cea
Brooke Perry and Devin Sweeting <devin@>
2019-01-22 15:23:29 -0800

    create recipe-box app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Recipe } from './models/recipe.model';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +9,26 @@ import { Recipe } from './models/recipe.model';
 })
 export class AppComponent {
   title = 'recipe-box';
-  currentFocus: string = 'A box for all your recipes'
+  currentFocus: string = 'A box for all your recipes';
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
 
   recipes: Recipe[] = [
-    new Recipe('Mac & Cheese', 'A perfect blend of fine cheddar and noodles', ['cheddar', 'noodles', 'bacon bits', 'truffles']),
-    new Recipe('Banana Bread', 'A harmonious blend of banana and bread', ['banana', 'flour', 'almond milk', 'butter', 'maple syrup'])
+    new Recipe('Mac & Cheese', 'A perfect blend of fine cheddar and noodles', ['cheddar', 'noodles', 'bacon bits', 'truffles'], 3),
+    new Recipe('Banana Bread', 'A harmonious blend of banana and bread', ['banana', 'flour', 'almond milk', 'butter', 'maple syrup'], 1)
   ]
 
+  selectedRecipe: Recipe = this.recipes[0];
+
+  priorityColor(currentRecipe){
+    if (currentRecipe.priority === 3){
+      return "bg-danger";
+    } else if (currentRecipe.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,4 +1,4 @@
 export class Recipe {
   public served: boolean = false;
-  constructor(public title: string, public description: string, public ingredients: array) { }
+  constructor(public title: string, public description: string, public ingredients: string[], public priority: number) { }
 }
