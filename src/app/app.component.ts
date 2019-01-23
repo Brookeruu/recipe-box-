54071d46f54d744838abbf65bd2cbbe910a26858
Brooke Perry and Devin Sweeting <brooke@>
2019-01-22 16:18:41 -0800

    Hide and show elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,19 @@ export class AppComponent {
     this.selectedRecipe = clickedRecipe;
   }
 
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
   recipes: Recipe[] = [
     new Recipe('Mac & Cheese', 'A perfect blend of fine cheddar and noodles', ['cheddar', 'noodles', 'bacon bits', 'truffles'], 3),
+
+    new Recipe('PB&J', 'A wonderful blend of artisan bread, fresh preserved fruits, and crushed nut', ['bread', 'jam', 'peanut better'], 2),
+
     new Recipe('Banana Bread', 'A harmonious blend of banana and bread', ['banana', 'flour', 'almond milk', 'butter', 'maple syrup'], 1)
   ]
 
-  selectedRecipe: Recipe = this.recipes[0];
+  selectedRecipe = null;
 
   priorityColor(currentRecipe){
     if (currentRecipe.priority === 3){
